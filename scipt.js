3cdd644804641af0e36b32c27b02e350a59f974c
Aleks Bozek <aleksbozek@Alekss-iMac.local>
2020-08-18 15:43:49 -0400

    generating next and previous photos as thumbnails.  Going to work on click functionality coupled with cycling through the array via next and previous

--- scipt.js
+++ scipt.js
@@ -13,8 +13,15 @@ async function getPic(year, month, day, camera) {
     const rndm = Math.floor((Math.random() * pictures.length))
     console.log(rndm)
     const rndmPhoto = pictures[rndm]
-    console.log(rndmPhoto)
-    displayPhoto(rndmPhoto)
+    let picture = `<img src=${rndmPhoto.img_src} alt="Photo ID #${rndmPhoto.id}" style="width: 85vw; height: auto">`
+    document.querySelector('#display').insertAdjacentHTML('beforeend', picture)
+    const nextPic = pictures[rndm - 1]
+    const prevPic = pictures[rndm + 1]
+    let pictureN = `<img src=${nextPic.img_src} alt="Photo ID #${nextPic.id}" style="width: 15vw; height: 15vh">`
+    document.querySelector('.up').insertAdjacentHTML('beforeend', pictureN)
+    let pictureP = `<img src=${prevPic.img_src} alt="Photo ID #${prevPic.id}" style="width: 15vw; height: 15vh">`
+    document.querySelector('.down').insertAdjacentHTML('beforeend', pictureP)
+
     return pictures
   } catch (error) {
     console.log(`Error: ${error}`)
@@ -22,23 +29,15 @@ async function getPic(year, month, day, camera) {
 }
 
 function removePicture() {
-   const photo = document.querySelector('#display')
+  const photo = document.querySelector('#display')
+  const photo2 = document.querySelector('.up')
+  const photo3 = document.querySelector('.down')
   while (photo.lastChild) {
     photo.removeChild(photo.lastChild)
+    photo2.removeChild(photo2.lastChild)
+    photo3.removeChild(photo3.lastChild)
   }
 }
-
-const displayPhoto = (photo) => {
-  let picture = `
-    <img src=${photo.img_src} alt="Photo ID #${photo.id}" style="width: 65vw; height: auto">
-`
-// style="width: 500px; height: auto;"
-  document.querySelector('#display').insertAdjacentHTML('beforeend', picture)
-  
-  console.log(photo)
-  
-}
-
 
 const button = document.querySelector('button')
 
