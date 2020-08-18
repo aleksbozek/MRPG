bfe69de6f666161c23a2bd191947f394cfab4c8b
Aleks Bozek <aleksbozek@Alekss-iMac.local>
2020-08-18 11:23:37 -0400

    displays a random photo from camera and day specified = true

--- scipt.js
+++ scipt.js
@@ -2,13 +2,20 @@
 //
 
 async function getPic(year, month, day, camera) {
-  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${year}-${month}-${day}&camera=${camera}&api_key=DEMO_KEY`
-
+  // const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${year}-${month}-${day}&camera=${camera}&api_key=DEMO_KEY`
+  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${year}-${month}-${day}&camera=${camera}&api_key=OY0d1uLC5e9CAOmUwsog61HM4LT5YdDYU0Ngtq73`
   try {
     removePicture()
     const res = await axios.get(url)
     console.log(res)
-    return res
+    let pictures = res.data.photos
+    console.log(pictures)
+    const rndm = Math.floor((Math.random() * pictures.length))
+    console.log(rndm)
+    const rndmPhoto = pictures[rndm]
+    console.log(rndmPhoto)
+    displayPhoto(rndmPhoto)
+    return pictures
   } catch (error) {
     console.log(`Error: ${error}`)
   }
@@ -20,6 +27,16 @@ function removePicture() {
     photo.removeChild(photo.lastChild)
   }
 }
+
+const displayPhoto = (photo) => {
+  let picture = `
+    <img src=${photo.img_src} alt="Photo ID #${photo.id}">
+`
+// style="width: 500px; height: auto;"
+  document.querySelector('#display').insertAdjacentHTML('beforeend', picture)
+  console.log(photo)
+}
+
 
 const button = document.querySelector('button')
 
@@ -33,10 +50,11 @@ button.addEventListener('click', (e) => {
   getPic(y, m, d, c)
 })
 
+
+
 // const monthSelected = document.getElementById('#select-month')
 
 // monthSelected.addEventListener('change', () => {
-  
-// This was going to be where days was changed between 31,30,29,28
+// This was going to be where days dropdown options changed based on month
 
 // })
